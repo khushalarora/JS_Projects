eaf69c00c64f5493a59a69d8f21c0096666be038
Khushal Arora <khushalcosmos26@gmail.com>
2025-03-07 11:50:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
     const projects = ["Split Landing Page", "Blurry Loading", "Form Input Wave", "Hidden Search", "Progress Steps", "Expanding Cards", "Rotating Navigation", "Scroll Animation", "Sound Board"];
-
     const list = document.getElementById("project-list");
     projects.forEach((project) => {
         const li = document.createElement("li");
